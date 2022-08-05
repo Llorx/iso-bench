c5a4f60612ea63d67b74611f05a2b6ecbf0d4f3b
Jorge Fuentes <jorge.fuentes@airtek.es>
2022-08-05 15:12:41 +0200

    ADD: Rollup build

---
+++ types/index.d.ts
@@ -0,0 +1,38 @@
+declare type Copy<T> = T extends Array<T> ? T : T;
+declare namespace IsoBench {
+    enum STRINGS {
+        WORSE = "WORSE",
+        BEST = "BEST",
+        COMPLETED = "[TESTS COMPLETED]"
+    }
+    type ScopeOptions = {
+        parallel?: number;
+        ms?: number;
+    };
+    class Scope<T_ARGS extends any[], T_SCOPE extends readonly any[]> {
+        private _args;
+        private _setup;
+        private _scripts;
+        private _doneScripts;
+        private _loggedScripts;
+        private _logData;
+        private _running;
+        private _endCb;
+        readonly options: Required<ScopeOptions>;
+        started: boolean;
+        constructor(options?: ScopeOptions, _setup?: (...args: Copy<T_ARGS>) => Promise<T_SCOPE> | T_SCOPE, ...args: T_ARGS);
+        add(name: string, cb: (...args: T_SCOPE) => any): this;
+        log(...log: any[]): this;
+        output(...log: any[]): this;
+        result(...log: any[]): this;
+        run(): Promise<void>;
+        private _logPack;
+        private _checkOutput;
+        private _next;
+        private _getWorkerScript;
+        private _checkDataResult;
+        private _runWorker;
+    }
+}
+
+export { IsoBench };
